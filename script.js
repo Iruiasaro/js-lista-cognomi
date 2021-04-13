2ad2e19d7dd90f32cbde7b48c0ffdc9ec065990f
Iruiasaro <brunettirosario@gmail.com>
2021-04-13 17:09:20 +0200

    ordinamento lista e posizione umana

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ var surname = ['Dida', 'Costacurta', 'Nesta', 'Maldini', 'Kaladze', 'Gattuso', '
 //prompt inserimento proprio cognome
 var inputSurname = prompt("Inserisci il tuo Cognome");
 
+//inseriamo il cognome indicato all'interno della lista
 surname.push(inputSurname);
 
 
+//rendiamo la lista di carattere minuscolo qualsiasi sia il carattere inserito dall'utente
+for (i = 0; i < surname.length; i++) {
+    surname[i] = surname[i].toLowerCase();
+}
 
+//stampa la lista in ordine alfabetico utilizzando .sort
+console.log(surname.sort());
+
+//indichiamo la posizione umana del cognome inserito
+console.log(surname.indexOf(inputSurname.toLowerCase()) + 1);
