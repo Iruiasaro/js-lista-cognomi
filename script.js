e544975b16cc9d0291c555409182e5ad866390d4
Iruiasaro <brunettirosario@gmail.com>
2021-04-13 16:29:52 +0200

    array più richiesta cognome

---
+++ script.js
@@ -0,0 +1,10 @@
+//creazione Array Cognomi
+var surname = ['Dida', 'Costacurta', 'Nesta', 'Maldini', 'Kaladze', 'Gattuso', 'Pirlo', 'Seedorf', 'Rui Costa', 'Shevchenko', 'Inzaghi'];
+
+//prompt inserimento proprio cognome
+var inputSurname = prompt("Inserisci il tuo Cognome");
+
+surname.push(inputSurname);
+
+
+
